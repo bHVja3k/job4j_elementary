d6c5d98d0eaec8b8e32b1ea4daedad99772ce6b1
meonoeep <meonoeek@gmail.com>
2020-03-31 15:51:27 +0300

    6. Идеальный вес.[#264102]

---
+++ src/ru/job4j/converter/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+public class Fit {
+
+    public static double manWeight(double height) {
+        return height * 1.15;
+    }
+
+
+    public static double womanWeight(double height) {
+        return height * 1.15;
+    }
+
+    public static void main(String[] args) {
+        double man = Fit.manWeight(100);
+        double woman = Fit.womanWeight(110);
+        System.out.println("Man 100 is " + man);
+        System.out.println("Woman 110 is " + woman);
+    }
+
+}
